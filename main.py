e94e49269c612ad301a25364c45ed5d93a5805c7
PureFallen <github@zelsi.us>
2022-04-17 06:01:17 +0200

    Add Support for Enemies killed during Encore

--- main.py
+++ main.py
@@ -70,41 +70,36 @@ def chat_listener(f, old_count, new_count):
         # Legend: ['<DATE>T<TIME>', '<MESSAGE_ID>', <'CHAT_TYPE'>, '<PLAYER_ID>', '<PLAYER_NAME>', '<MESSAGE>']
         line_list = line.split("\t")
         if line_list[3] == PLAYER_ID:
+            global ENEMIES
             global PSE
 
             msg = line_list[5].replace("\n", "")
             if msg == BURST_MSG:
+                ENEMIES = -1
                 PSE = True
                 prints.print_info("PSE Burst detected. Logging Enemy Kills...")
             elif msg == CLIMAX_MSG:
-                global ENEMIES
-
-                PSE = False
-                prints.print_info("PSE Climax detected.")
-                prints.print_info(f"Enemies killed during PSE: {BColors.LIGHT_PURPLE}{ENEMIES}")
-                ENEMIES = -1
+                if PSE:
+                    PSE = False
+                    prints.print_info("PSE Climax detected.")
+                    prints.print_info(f"Enemies killed during PSE: {BColors.LIGHT_PURPLE}{ENEMIES}")
+                else:
+                    prints.print_info("Additional PSE Climax detected. Is that an Encore, or did you change rooms?")
+                    prints.print_info(f"Enemies killed since last PSE Climax: {BColors.LIGHT_PURPLE}{encore_routine()}")
 
 
 def action_loop():
+    # Get initial values
     date, path, old_count = init("ActionLog")
 
     while True:
         if PSE:
             global ENEMIES
             if ENEMIES == -1:
-                date, path, old_count = init("ActionLog")
+                date, path, _ = init("ActionLog")
 
                 # PSE Auto Chat is delayed; Look for latest cleared Trial
-                f = open(path, 'r', encoding='utf-16')
-                lines = f.readlines()
-                for i, line in enumerate(lines):
-                    # Check function action_listener for Legend.
-                    line_list = line.split("\t")
-                    if len(line_list) >= 7:
-                        amount = line_list[6].replace("\n", "")
-                        if amount == "N-Meseta(1000)" or amount == "N-Meseta(1500)":
-                            prints.print_info(i)
-                            old_count = i
+                old_count = latest_trial(path)
 
                 ENEMIES += 1
 
@@ -136,6 +131,45 @@ def action_listener(f, old_count, new_count):
             if amount.startswith("N-Meseta"):
                 global ENEMIES
                 ENEMIES += 1
+
+
+def encore_routine():
+    encore_enemies = 0
+
+    _, path, _ = init("ActionLog")
+    old_count = latest_trial(path)
+
+    f = open(path, 'r', encoding='utf-16')
+    new_count = sum(1 for _ in f) - 1
+    f.seek(0, 0)
+    lines = f.readlines()[-(new_count - old_count):]
+
+    for line in lines:
+        line_list = line.split("\t")
+        if len(line_list) >= 7:
+            amount = line_list[6].replace("\n", "")
+            if amount.startswith("N-Meseta"):
+                encore_enemies += 1
+
+    encore_enemies -= ENEMIES
+    f.close()
+    return encore_enemies
+
+
+def latest_trial(path):
+    old_count = 0
+
+    f = open(path, 'r', encoding='utf-16')
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        # Check function action_listener for Legend.
+        line_list = line.split("\t")
+        if len(line_list) >= 7:
+            amount = line_list[6].replace("\n", "")
+            if amount == "N-Meseta(1000)" or amount == "N-Meseta(1500)":
+                old_count = i
+
+    return old_count
 
 
 if __name__ == "__main__":
